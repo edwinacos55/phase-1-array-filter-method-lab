cd7351058ac801c59deec337fa44acc06282a6e4
edwinacos55 <edwinacos55@gmail.com>
2023-01-08 20:05:38 -0500

    commit tree 32248178d73a111ec11c0378c0f791d7cb26c3a3

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching (drivers, string) {
+  return  drivers.filter(element => element.toUpperCase()=== string.toUpperCase());
+}
+
+function fuzzyMatch(drivers, firstLetter) {
+    return  drivers.filter(element => element.slice(0, 2) === firstLetter);
+}
+
+function matchName (drivers, names) {
+    return drivers.filter(element => element.name===names);
+}
